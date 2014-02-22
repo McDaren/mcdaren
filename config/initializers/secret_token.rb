192faf46305067b57e88eef56db2fe5a4165b49f
Alan Chang <alan@allpim.com>
2014-02-22 22:05:59 +0800

    New Rails Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mcdaren::Application.config.secret_key_base = '9d0e173ebabeffaa53a8da4dd87dcfe51b6b432788eb741148fb8d1d9d97b76626344562e00c367568f0cc5a7c94296a772a411c6650e00a875a965237bf9a69'
